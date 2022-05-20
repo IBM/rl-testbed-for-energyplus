40e8c6aa7da7044b18fab548e85891bd53469290
antoine_galataud <antoine@foobot.io>
2022-05-20 21:19:07 +0200

    make experiment runnable by tune

--- ray_energyplus/ppo/run_energyplus.py
+++ ray_energyplus/ppo/run_energyplus.py
@@ -4,7 +4,7 @@ import sys
 
 import gym
 from ray import tune
-from ray.tune import register_env
+from ray.tune import register_env, Experiment
 
 # from baselines_energyplus.bench import Monitor
 from baselines_energyplus.common.energyplus_util import (
@@ -31,10 +31,10 @@ def env_creator(seed):
 
 
 def train(env_id, num_timesteps, seed):
-    log_dir = os.path.join(
-        energyplus_logbase_dir(),
-        datetime.datetime.now().strftime("openai-%Y-%m-%d-%H-%M-%S-%f")
-    )
+    experiment_id = datetime.datetime.now().strftime("ray-%Y-%m-%d-%H-%M-%S-%f")
+
+    log_base = energyplus_logbase_dir()
+    log_dir = os.path.join(log_base, experiment_id)
     if not os.path.exists(log_dir + '/output'):
         os.makedirs(log_dir + '/output')
     os.environ["ENERGYPLUS_LOG"] = log_dir
@@ -55,24 +55,32 @@ def train(env_id, num_timesteps, seed):
     register_env(env_id, env)
 
     config = {
+        "run": "PPO",
         "env": env_id,
-        "env_config": {
-            "energyplus_file": energyplus_file,
-            "model_file": model,
-            "weather_file": weather,
-            "log_dir": log_dir
+        "config": {
+            "framework": "tf2",
+            # see Ray PPO config for all options
+            "gamma": 0.99,
+            "kl_coeff": 0.2,
+            # arguments passed to EnergyPlusEnv
+            "env_config": {
+                "energyplus_file": energyplus_file,
+                "model_file": model,
+                "weather_file": weather,
+                "log_dir": log_dir
+            },
         },
-        "framework": "tf2",
-        # see Ray PPO config for all options
-        "gamma": 0.99,
-        "kl_coeff": 0.2
+        "checkpoint_freq": 2,
+        "checkpoint_at_end": True,
+        "local_dir": log_base,
     }
 
     stop = {
         "timesteps_total": num_timesteps
     }
 
-    results = tune.run("PPO", stop=stop, config=config, verbose=2)
+    experiment = Experiment.from_json(experiment_id, config)
+    results = tune.run(run_or_experiment=experiment, stop=stop, config=config, verbose=2)
     print(results)
 
 
