6648505882f80585263965e8c63723a4a886edf3
antoine_galataud <antoine@foobot.io>
2022-05-20 21:18:40 +0200

    simplify env making

--- baselines_energyplus/common/plot_energyplus.py
+++ baselines_energyplus/common/plot_energyplus.py
@@ -3,8 +3,10 @@
 import argparse
 import os
 
+import gym
+
 from baselines_energyplus.common.energyplus_util import make_energyplus_env, energyplus_locate_log_dir
-import gym_energyplus
+
 
 def plot_energyplus_arg_parser():
     """
@@ -18,25 +20,34 @@ def plot_energyplus_arg_parser():
     parser.add_argument('--dump-episodes', '-D', help='Dump episodes', action='store_true')
     return parser
 
+
 def energyplus_plot(env_id, log_dir='', csv_file='', dump_timesteps=False, dump_episodes=False):
     if log_dir is not '' and csv_file is not '':
         print('Either log directory or csv file can be specified')
         os.exit(1)
     if log_dir is '' and csv_file is '':
         log_dir = energyplus_locate_log_dir()
-    env = make_energyplus_env(env_id, 0)
+    env = gym.make(env_id)
     assert sum([dump_timesteps, dump_episodes]) != 2
     if dump_timesteps:
-        env.env.dump_timesteps(log_dir=log_dir, csv_file=csv_file)
+        env.dump_timesteps(log_dir=log_dir, csv_file=csv_file)
     elif dump_episodes:
-        env.env.dump_episodes(log_dir=log_dir, csv_file=csv_file)
+        env.dump_episodes(log_dir=log_dir, csv_file=csv_file)
     else:
-        env.env.plot(log_dir=log_dir, csv_file=csv_file)
+        env.plot(log_dir=log_dir, csv_file=csv_file)
     env.close()
+
 
 def main():
     args = plot_energyplus_arg_parser().parse_args()
-    energyplus_plot(args.env, log_dir=args.log_dir, csv_file=args.csv_file, dump_timesteps=args.dump_timesteps, dump_episodes=args.dump_episodes)
+    energyplus_plot(
+        args.env,
+        log_dir=args.log_dir,
+        csv_file=args.csv_file,
+        dump_timesteps=args.dump_timesteps,
+        dump_episodes=args.dump_episodes
+    )
+
 
 if __name__ == '__main__':
     main()
