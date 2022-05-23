e0b957bf19ae776ff7faa088cd35efd19aa94cc4
antoine_galataud <antoine@foobot.io>
2022-05-23 12:04:33 +0200

    [WIP] tune experiment

--- ray_energyplus/ppo/run_energyplus.py
+++ ray_energyplus/ppo/run_energyplus.py
@@ -2,7 +2,6 @@
 # noinspection PyUnresolvedReferences
 import sys
 
-import gym
 from ray import tune
 from ray.tune import register_env, Experiment
 
@@ -15,6 +14,7 @@ import os
 import datetime
 
 from gym_energyplus.envs import EnergyPlusEnv
+from ray_energyplus.common.callbacks import MonitorCallbacks
 
 
 def env_creator(seed):
@@ -51,17 +51,36 @@ def train(env_id, num_timesteps, seed):
 
     energyplus_file = os.getenv('ENERGYPLUS')
 
+    # gym env registration
     env = env_creator(seed)
     register_env(env_id, env)
 
+    # experiment configuration, including alg params
     config = {
         "run": "PPO",
         "env": env_id,
         "config": {
             "framework": "tf2",
             # see Ray PPO config for all options
-            "gamma": 0.99,
+            "gamma": 0.85,
             "kl_coeff": 0.2,
+            # increase this if more CPUs are available
+            "num_workers": 2,
+            # small batches speed up training
+            # you may want to increase these values for stability
+            "train_batch_size": 4000,
+            "rollout_fragment_length": 200,
+            # very large reward values require larger
+            # value function clipping value, otherwise would take
+            # large number of iterations to converge
+            "vf_clip_param": 1000,
+            # it's an (almost) continuous control problem
+            # we split each year-long episode into 1-day logical episodes
+            # so rewards are computed
+            "horizon": 96,
+            # do not force env reset when horizon is hit
+            "soft_horizon": True,
+            "batch_mode": "truncate_episodes",
             # arguments passed to EnergyPlusEnv
             "env_config": {
                 "energyplus_file": energyplus_file,
@@ -75,13 +94,23 @@ def train(env_id, num_timesteps, seed):
         "local_dir": log_base,
     }
 
+    # experiment stop conditions
     stop = {
         "timesteps_total": num_timesteps
     }
 
+    # callbacks
+    tune_callbacks = [MonitorCallbacks(filename=log_dir, env_id=env_id)]
+
     experiment = Experiment.from_json(experiment_id, config)
-    results = tune.run(run_or_experiment=experiment, stop=stop, config=config, verbose=2)
-    print(results)
+    results = tune.run(
+        run_or_experiment=experiment,
+        stop=stop,
+        config=config,
+        callbacks=tune_callbacks,
+        verbose=2
+    )
+    print(results.results_df)
 
 
 def main():
